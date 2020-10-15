2b133d680e4e398b3cf41996a7792f76c2c0b273
Davide Icardi <davide.icardi@gmail.com>
2020-10-15 15:00:58 +0200

    add generic avro record support (#29)
    * add generic avro record support

---
+++ kaa/src/main/scala/com/davideicardi/kaa/avro/GenericAvroBinarySerializer.scala
@@ -0,0 +1,41 @@
+package com.davideicardi.kaa.avro
+
+import org.apache.avro._
+import java.io.ByteArrayOutputStream
+
+import org.apache.avro.file.SeekableByteArrayInput
+import org.apache.avro.generic.{GenericDatumReader, GenericDatumWriter, GenericRecord}
+import org.apache.avro.io.{DecoderFactory, EncoderFactory}
+
+class GenericAvroBinarySerializer() {
+  def write(record: GenericRecord): Array[Byte] = {
+    val datumWriter = new GenericDatumWriter[GenericRecord](record.getSchema)
+
+    val outputStream = new ByteArrayOutputStream()
+    try {
+      val encoder = EncoderFactory.get.binaryEncoder(outputStream, null)
+
+      datumWriter.write(record, encoder)
+
+      encoder.flush()
+
+      outputStream.toByteArray
+    } finally {
+      outputStream.close()
+    }
+  }
+
+  def read(bytes: Array[Byte], writerSchema: Schema, readerSchema: Option[Schema] = None): GenericRecord = {
+    val datumReader = new GenericDatumReader[GenericRecord](
+      writerSchema,
+      readerSchema.getOrElse(writerSchema)
+    )
+    val inputStream = new SeekableByteArrayInput(bytes)
+    try {
+      val decoder = DecoderFactory.get.binaryDecoder(inputStream, null)
+      datumReader.read(null, decoder)
+    } finally {
+      inputStream.close()
+    }
+  }
+}
---
+++ kaa/src/test/scala/kaa/darwin/GenericAvroSingleObjectSerializerSpec.scala
@@ -0,0 +1,83 @@
+package kaa.darwin
+
+import com.davideicardi.kaa.SchemaId
+import com.davideicardi.kaa.avro.{AvroSingleObjectEncoding, GenericAvroBinarySerializer, GenericAvroSingleObjectSerializer}
+import com.davideicardi.kaa.test.TestSchemaRegistry
+import org.apache.avro.generic.GenericData
+import org.apache.avro.{Schema, SchemaNormalization}
+import org.scalatest.flatspec._
+import org.scalatest.matchers._
+
+class GenericAvroSingleObjectSerializerSpec extends AnyFlatSpec with should.Matchers {
+
+  val registry = new TestSchemaRegistry
+
+  val singleObjectSerializer = new GenericAvroSingleObjectSerializer(registry)
+
+  private val schemaV1 = new Schema.Parser().parse(
+    """
+      |{
+      | "namespace": "test.avro",
+      | "type": "record",
+      | "name": "FooUser",
+      | "fields": [
+      |     {"name": "name", "type": "string"}
+      | ]
+      |}
+      """.stripMargin
+  )
+
+  private val schemaV2 = new Schema.Parser().parse(
+    """
+      |{
+      | "namespace": "test.avro",
+      | "type": "record",
+      | "name": "FooUser",
+      | "fields": [
+      |     {"name": "name", "type": "string"},
+      |     {"name": "age",  "type": "int", "default": 25}
+      | ]
+      |}
+      """.stripMargin
+  )
+
+  "GenericAvroSingleObjectSerializer" should "serialize and deserialize a record" in {
+    val record = new GenericData.Record(schemaV1)
+    record.put("name", "foo")
+
+    val encoded = singleObjectSerializer.serialize(record)
+    val decoded = singleObjectSerializer.deserialize(encoded)
+
+    decoded.get("name").toString should equal ("foo")
+  }
+
+  it should "serialize and deserialize a record passing an expected record" in {
+    val record = new GenericData.Record(schemaV1)
+    record.put("name", "foo")
+
+    val encoded = singleObjectSerializer.serialize(record)
+    val decoded = singleObjectSerializer.deserialize(encoded, Some(schemaV2))
+
+    decoded.get("name").toString should equal ("foo")
+    decoded.get("age") should equal (25)
+  }
+
+  it should "serialize a class with a single object encoding" in {
+    val record = new GenericData.Record(schemaV1)
+    record.put("name", "foo")
+
+    val encoded = singleObjectSerializer.serialize(record)
+
+    val (schemaId, bin) = AvroSingleObjectEncoding.decode(encoded)
+    val binarySerializer = new GenericAvroBinarySerializer
+
+    schemaId should be (AvroUtils.calcFingerprint(schemaV1))
+    bin should be (binarySerializer.write(record))
+  }
+
+  object AvroUtils {
+    def calcFingerprint(schema: Schema): SchemaId = {
+      SchemaId(SchemaNormalization.parsingFingerprint64(schema))
+    }
+  }
+}
