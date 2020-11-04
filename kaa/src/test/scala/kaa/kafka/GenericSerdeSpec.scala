c0176d572b911b85d5823f0a1222e3b0c60b3605
Davide Icardi <davide.icardi@gmail.com>
2020-11-04 19:50:11 +0100

    handle null in genericserde for tombstones handling

---
+++ kaa/src/test/scala/kaa/kafka/GenericSerdeSpec.scala
@@ -0,0 +1,53 @@
+package com.davideicardi.kaa.darwin
+
+import org.scalatest._
+import flatspec._
+import matchers._
+import com.davideicardi.kaa.kafka.GenericSerde
+import com.davideicardi.kaa.test.TestSchemaRegistry
+
+class GenericSerdeSpec extends AnyFlatSpec with should.Matchers {
+
+  val registry = new TestSchemaRegistry
+
+  "GenericSerde" should "serialize and deserialize a case class" in {
+    val target = new GenericSerde[FooUser](registry)
+
+    val expected = FooUser("foo")
+    val bytes = target.serialize("topic", expected)
+    val result = target.deserialize("topic", bytes)
+
+    result should equal (expected)
+  }
+
+  it should "serialize and deserialize an Option Some" in {
+    val target = new GenericSerde[Option[FooUser]](registry)
+
+    val expected = Some(FooUser("foo"))
+    val bytes = target.serialize("topic", expected)
+    val result = target.deserialize("topic", bytes)
+    result should equal (expected)
+  }
+
+  it should "serialize and deserialize an Option None" in {
+    val target = new GenericSerde[Option[FooUser]](registry)
+
+    val expected: Option[FooUser] = None
+    val bytes = target.serialize("topic", expected)
+    val result = target.deserialize("topic", bytes)
+    result should equal (expected)
+  }
+
+  it should "serialize and deserialize a null" in {
+    val target = new GenericSerde[FooUser](registry)
+
+    val expected: FooUser = null
+    val bytes = target.serialize("topic", expected)
+    val result = target.deserialize("topic", bytes)
+
+    result should equal (expected)
+  }
+
+  case class FooUser (name: String) {
+  }
+}
