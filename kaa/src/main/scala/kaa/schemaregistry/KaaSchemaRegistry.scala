2dacd4b43680924c42977c7daf91ba8435adc4e3
Davide Icardi <davide.icardi@gmail.com>
2021-03-26 13:47:29 +0100

    WIP

--- kaa/src/main/scala/kaa/schemaregistry/KaaSchemaRegistry.scala
+++ kaa/src/main/scala/kaa/schemaregistry/KaaSchemaRegistry.scala
@@ -14,13 +14,12 @@ import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.common.serialization.{LongDeserializer, StringDeserializer}
 import org.apache.kafka.common.serialization.{LongSerializer, StringSerializer}
 import org.apache.kafka.clients.CommonClientConfigs
-
 import kaa.schemaregistry.utils.Retry
 import kaa.schemaregistry.utils.RetryConfig
 import kaa.schemaregistry.KaaSchemaRegistry._
 
 import scala.concurrent.duration._
-import java.util.concurrent.atomic.AtomicBoolean
+import java.util.concurrent.atomic.{AtomicBoolean, AtomicReference}
 import scala.concurrent._
 
 object KaaSchemaRegistry {
@@ -30,9 +29,9 @@ object KaaSchemaRegistry {
   val DEFAULT_RETRY_CONFIG: RetryConfig = RetryConfig(5, 2.second)
 
   def createProps(
-            brokers: String,
-            clientId: String = DEFAULT_CLIENT_ID,
-          ): Properties = {
+                   brokers: String,
+                   clientId: String = DEFAULT_CLIENT_ID,
+                 ): Properties = {
     val props = new Properties()
     props.put(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, brokers)
     props.put(CommonClientConfigs.CLIENT_ID_CONFIG, clientId)
@@ -41,69 +40,69 @@ object KaaSchemaRegistry {
 }
 
 class KaaSchemaRegistry(
-  producerProps: Properties,
-  consumerProps: Properties,
-  topic: String = DEFAULT_TOPIC_NAME,
-  pollInterval: Duration = DEFAULT_POLL_INTERVAL,
-  getRetry: RetryConfig = DEFAULT_RETRY_CONFIG
-) extends SchemaRegistry {
+                         producerProps: Properties,
+                         consumerProps: Properties,
+                         topic: String = DEFAULT_TOPIC_NAME,
+                         pollInterval: Duration = DEFAULT_POLL_INTERVAL,
+                         getRetry: RetryConfig = DEFAULT_RETRY_CONFIG,
+                         onError: Exception => Unit
+                       ) extends SchemaRegistry {
 
   def this(producerProps: Properties,
-           consumerProps: Properties) = {
+           consumerProps: Properties,
+           onError: Exception => Unit,
+          ) = {
     this(
       producerProps = producerProps,
       consumerProps = consumerProps,
       topic = DEFAULT_TOPIC_NAME,
       pollInterval = DEFAULT_POLL_INTERVAL,
       getRetry = DEFAULT_RETRY_CONFIG,
+      onError,
     )
   }
-  def this(brokers: String) = {
+
+  def this(brokers: String, onError: Exception => Unit) = {
     this(
       producerProps = createProps(brokers),
       consumerProps = createProps(brokers),
+      onError,
     )
   }
 
-  implicit private val ec: ExecutionContextExecutor = ExecutionContext.global
-
-  private val producer = createProducer()
-  private val consumer = createConsumer()
   private val cache: Cache[Long, String] = Scaffeine().build[Long, String]()
   private val stopping = new AtomicBoolean(false)
-  private val startConsumerFuture = startConsumer()
+  private val producer = new AtomicReference[Option[KaaProducer]]
+  private val consumer = new AtomicReference[Option[KaaConsumer]]
 
-  private def startConsumer(): Future[Unit] = Future {
-    consumer.subscribe(Collections.singletonList(topic))
-    val jPollInterval = JavaDuration.ofMillis(pollInterval.toMillis)
-    while (!stopping.get()) {
-      val records = consumer.poll(jPollInterval)
-
-      records.forEach(record => {
-        cache.put(record.key(), record.value())
-      })
-    }
+  def start()(implicit ec: ExecutionContext): Unit = {
+    if (!stopping.get())
+      throw InvalidStateException("Schema registry is stopping")
+    if (!producer.compareAndSet(None, Some(new KaaProducer())))
+      throw InvalidStateException("Schema registry already started")
+    if (!consumer.compareAndSet(None, Some(new KaaConsumer())))
+      throw InvalidStateException("Schema registry already started")
   }
 
   def close(maxWait: Duration = 10.seconds): Unit = {
     stopping.set(true)
-    Await.result(startConsumerFuture, maxWait)
-    val maxWaitJava = JavaDuration.ofMillis(maxWait.toMillis)
-    consumer.close(maxWaitJava)
-    producer.close(maxWaitJava)
+    consumer.getAndSet(None) match {
+      case Some(c) => c.close(maxWait)
+      case None =>
+    }
+    producer.getAndSet(None) match {
+      case Some(p) => p.close(maxWait)
+      case None =>
+    }
+    cache.cleanUp()
+    stopping.set(false)
   }
 
   override def put(schema: Schema): SchemaId = {
-    if (stopping.get()) throw new UnsupportedOperationException("KaaSchemaRegistry is not available")
-
-    val fingerprint = SchemaNormalization.parsingFingerprint64(schema)
-
-    if (cache.getIfPresent(fingerprint).isEmpty) {
-      val record = new ProducerRecord[java.lang.Long, String](topic, fingerprint, schema.toString())
-      producer.send(record).get()
+    producer.get() match {
+      case Some(p) => p.put(schema)
+      case None => throw InvalidStateException("Schema registry not started")
     }
-
-    SchemaId(fingerprint)
   }
 
   override def get(id: SchemaId): Option[Schema] = {
@@ -113,28 +112,77 @@ class KaaSchemaRegistry(
     }
   }
 
-  protected def createConsumer(): KafkaConsumer[lang.Long, String] = {
-    new KafkaConsumer(fillConsumerProps(), new LongDeserializer(), new StringDeserializer())
+  class KaaConsumer()(implicit ec: ExecutionContext) {
+    private val consumer = createConsumer()
+    private val startConsumerFuture = startConsumer()
+
+    private def startConsumer(): Future[Unit] = Future {
+      try {
+        consumer.subscribe(Collections.singletonList(topic))
+        val jPollInterval = JavaDuration.ofMillis(pollInterval.toMillis)
+        while (!stopping.get()) {
+          val records = consumer.poll(jPollInterval)
+
+          records.forEach(record => {
+            cache.put(record.key(), record.value())
+          })
+        }
+      } catch {
+        case ex: Exception => onError(ex)
+      }
+    }
+
+    def close(maxWait: Duration): Unit = {
+      Await.result(startConsumerFuture, maxWait)
+      val maxWaitJava = JavaDuration.ofMillis(maxWait.toMillis)
+      consumer.close(maxWaitJava)
+    }
+
+    protected def createConsumer(): KafkaConsumer[lang.Long, String] = {
+      new KafkaConsumer(fillConsumerProps(), new LongDeserializer(), new StringDeserializer())
+    }
+
+    protected def fillConsumerProps(): Properties = {
+
+      consumerProps.putIfAbsent(ConsumerConfig.CLIENT_ID_CONFIG, KaaSchemaRegistry.DEFAULT_CLIENT_ID)
+
+      consumerProps.put(ConsumerConfig.GROUP_ID_CONFIG, UUID.randomUUID().toString)
+      consumerProps.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false")
+      consumerProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
+
+      consumerProps
+    }
   }
 
-  protected def fillConsumerProps(): Properties = {
+  class KaaProducer() {
+    private val producer = createProducer()
 
-    consumerProps.putIfAbsent(ConsumerConfig.CLIENT_ID_CONFIG, KaaSchemaRegistry.DEFAULT_CLIENT_ID)
+    def close(maxWait: Duration): Unit = {
+      val maxWaitJava = JavaDuration.ofMillis(maxWait.toMillis)
+      producer.close(maxWaitJava)
+    }
 
-    consumerProps.put(ConsumerConfig.GROUP_ID_CONFIG, UUID.randomUUID().toString)
-    consumerProps.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false")
-    consumerProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
+    def put(schema: Schema): SchemaId = {
+      if (stopping.get()) throw new UnsupportedOperationException("KaaSchemaRegistry is not available")
 
-    consumerProps
-  }
+      val fingerprint = SchemaNormalization.parsingFingerprint64(schema)
 
-  protected def createProducer(): KafkaProducer[lang.Long, String] = {
-    new KafkaProducer(fillProducerProps(), new LongSerializer(), new StringSerializer())
-  }
+      if (cache.getIfPresent(fingerprint).isEmpty) {
+        val record = new ProducerRecord[java.lang.Long, String](topic, fingerprint, schema.toString())
+        producer.send(record).get()
+      }
 
-  protected def fillProducerProps(): Properties = {
-    producerProps.putIfAbsent(ProducerConfig.CLIENT_ID_CONFIG, KaaSchemaRegistry.DEFAULT_CLIENT_ID)
+      SchemaId(fingerprint)
+    }
 
-    producerProps
+    protected def createProducer(): KafkaProducer[lang.Long, String] = {
+      new KafkaProducer(fillProducerProps(), new LongSerializer(), new StringSerializer())
+    }
+
+    protected def fillProducerProps(): Properties = {
+      producerProps.putIfAbsent(ProducerConfig.CLIENT_ID_CONFIG, KaaSchemaRegistry.DEFAULT_CLIENT_ID)
+
+      producerProps
+    }
   }
 }
